d0caac54083a9570ef958e5f618073b147b3ec53
Moniruzzaman <moniruzzaman@guardianlife.com.bd>
2022-01-01 13:55:09 +0600

    react install via vite, using ethereum & tailwind, navbar responsive, welcome configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {Navbar,Welcome,Footer,Services,Transactions,Loader} from './components/index';
+const App = () => {
+  return (
+    <div className="min-h-screen">
+       <div className='gradient-bg-welcome'>
+         <Navbar/>
+         <Welcome/>
+         </div>
+         <Services/>
+         <Transactions/>
+         <Footer/>
+    </div>
+  )
+}
+
+export default App;
